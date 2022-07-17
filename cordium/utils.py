1370c0ccdcbe6f81e3cf444fdea92fec102b2b30
Dorukyum <doruk.ak@hotmail.com>
2022-07-17 17:31:09 +0300

    Create MISSING

--- cordium/utils.py
+++ cordium/utils.py
@@ -1,6 +1,16 @@
+from typing import Any
+
+
 def try_snowflake(data, key: str) -> int | None:
     try:
         snowflake = data[key]
     except KeyError:
         return None
     return snowflake is not None and int(snowflake)
+
+
+class _MISSING:
+    """Represents a "missing" argument, not to be confused with `None`."""
+
+
+MISSING: Any = _MISSING()
